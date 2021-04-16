5aeefe8833edf58a7390c472634ed34358250cb4
Victor <vctr1990@gmail.com>
2021-04-16 19:57:23 +0200

    FateGo api database

--- FateGO/FateGO/urls.py
+++ FateGO/FateGO/urls.py
@@ -1,4 +1,4 @@
-"""webSite URL Configuration
+"""FateGO URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# Use include() to add paths from the catalog application
+from django.contrib import admin
 from django.urls import path
-from Game import views
 
 urlpatterns = [
-    path('', views.Game, name='game'),
+    path('admin/', admin.site.urls),
 ]
-
-
--- FateGO/Servant/apps.py
+++ FateGO/Servant/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GameConfig(AppConfig):
+class ServantConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Game'
+    name = 'Servant'

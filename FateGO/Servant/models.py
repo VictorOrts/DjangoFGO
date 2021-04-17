1216a6098f2ed8bcdf72e6851561369c21ea1443
Victor <vctr1990@gmail.com>
2021-04-17 02:08:47 +0200

    fgo visible html

--- FateGO/Servant/models.py
+++ FateGO/Servant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# "https://api.atlasacademy.io/basic/NA/servant/search?lang=en&name=Kama&className=assassin"
+class Servantch(models.Model):
+    unique_id  =  models.CharField(max_length=8, primary_key=True)
+    collectionNo = models.CharField(max_length=5)
+    name = models.CharField(max_length=30)
+    typeS = models.CharField(max_length=30)
+    flag = models.CharField(max_length=30)
+    className = models.CharField(max_length=30)
+    attribute = models.CharField(max_length=30)
+    rarity = models.CharField(max_length=1)
+    atkMax = models.CharField(max_length=5)
+    hpMax = models.CharField(max_length=5)
+    face = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- FateGO/Servant/views.py
+++ FateGO/Servant/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from .models import Servantch
+import requests
 # Create your views here.
+
+
+def Servant(request):
+    all_meals = {}
+    url = 'https://api.atlasacademy.io/export/NA/basic_servant.json'
+    response = requests.get(url)
+    data = response.json()
+    servant = data
+    print(servant)
+    for i in servant:
+        meal_data = Servantch(
+            unique_id  = i['id'],
+            collectionNo = i['collectionNo'],
+            name = i['name'],
+            typeS = i['type'],
+            flag = i['flag'],
+            className = i['className'],
+            attribute = i['attribute'],
+            rarity = i['rarity'],
+            atkMax = i['atkMax'],
+            hpMax = i['hpMax'],
+            face = i['face']
+        )
+        meal_data.save()
+        all_meals = Servantch.objects.all().order_by('collectionNo')
+
+    return render (request, 'main.html', { "all_meals": 
+    all_meals} )

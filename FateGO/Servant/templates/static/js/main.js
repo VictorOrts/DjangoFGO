8a138c3d213543d0ebb07fef763b84eb3fd09087
VictorOrts <vctr1990@gmail.com>
2021-05-08 03:11:06 +0200

    Improve info and detail ,added craft essence..

--- FateGO/Servant/templates/static/js/main.js
+++ FateGO/Servant/templates/static/js/main.js
@@ -1,3 +1,14 @@
 function collectDataSearch(){
 
 }
+
+function ChangeImage(image){
+    console.log(image);
+    var img = document.getElementById("image");
+    img.src=image;
+}
+function putFirstImage(aaaa){
+    console.log(aaaa);
+    var button = document.getElementById("stage1");
+    button.click();
+}
